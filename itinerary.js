388d34c89e0cd2f09af5b5d771625d8e799385bc
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-10-03 23:09:10 +0200

    Add console.log to display itinerary

--- itinerary.js
+++ itinerary.js
@@ -6,3 +6,5 @@ let plannedActivities = 3;
 let budget = "R4000";
 
 const itinerary = "The traveler " + travelerName + " is from " + country + " and will be staying for " + numberOfDaysStaying + " days at the " + hotelName + " and she has " + plannedActivities + " activities planned with a " + budget + " budget for the trip.";
+
+console.log(itinerary);
