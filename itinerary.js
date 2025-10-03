9003febbb382daca6ea70b8568f7c3c5d387a613
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-10-03 23:07:26 +0200

    Build itinerary string with concatenation

--- itinerary.js
+++ itinerary.js
@@ -4,3 +4,5 @@ let numberOfDaysStaying = 4;
 const hotelName = "Three-Stars Hotel";
 let plannedActivities = 3;
 let budget = "R4000";
+
+const itinerary = "The traveler " + travelerName + " is from " + country + " and will be staying for " + numberOfDaysStaying + " days at the " + hotelName + " and she has " + plannedActivities + " activities planned with a " + budget + " budget for the trip.";
